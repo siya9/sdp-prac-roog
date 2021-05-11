dc74dbeeb73fce36bab7f409a511d6709e56096c
Siyamthanda Hlomendlini <shlomendlini82@gmail.com>
2021-05-11 13:54:01 +0200

    printing student number

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -7,6 +7,7 @@ namespace Roog
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!"); //hello world print
+		Console.WriteLine("G19H4806"); //printing student number
         }
     }
 }
